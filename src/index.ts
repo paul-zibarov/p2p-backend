063725c0cc055c9d52644f0577c220cc82cda924
Paul Zibarov <paul.zibarov@gmail.com>
2022-06-23 02:33:31 +0300

    Add endpoints for fetching lots data.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+
 import { handleNewLotEvents, handleBuyLotEvents, handleCancelLotEvents } from "./services/EventHandler";
 import { sequelize } from "./db"
+import { getLotById, getLots } from './controllers/LotController';
+
+const app = express()
+const jsonParser = bodyParser.json();
+const port = process.env.PORT_API || 5000;
+
+app.get('/getLots', jsonParser, async (req, res) => {
+    try {
+        let lots = await getLots(req.body.page, req.body.countPerPage);
+        res.send(lots)
+    } catch(e) {
+        console.log(e)
+    }
+})
+
+app.get('/getLotById', jsonParser, async (req, res) => {
+    try {
+        let lot = await getLotById(req.body.lotId);
+        res.send(lot)
+    } catch(e) {
+        console.log(e)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 sequelize.sync().then(() => {
     console.log('Synchronized.');
