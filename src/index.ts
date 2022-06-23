c2e921c3d1699015f55a47b8b3a644747c36cc9e
Paul Zibarov <paul.zibarov@gmail.com>
2022-06-23 20:53:26 +0300

    Move request params from body to query. Update dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express'
-import bodyParser from 'body-parser';
 
 import { handleNewLotEvents, handleBuyLotEvents, handleCancelLotEvents } from "./services/EventHandler";
 import { sequelize } from "./db"
 import { getAllActiveLotsCount, getLotById, getLots } from './controllers/LotController';
 
 const app = express()
-const jsonParser = bodyParser.json();
 const port = process.env.PORT || 5000;
 
 app.get('/', async (req, res) => {
@@ -26,18 +24,18 @@ app.get('/getActiveLotsCount', async (req, res) => {
     }
 })
 
-app.get('/getActiveLots', jsonParser, async (req, res) => {
+app.get('/getActiveLots', async (req, res) => {
     try {
-        let lots = await getLots(req.body.page, req.body.countPerPage);
+        let lots = await getLots(Number(req.query.page), Number(req.query.countPerPage));
         res.send(lots)
     } catch(e) {
         console.log(e)
     }
 })
 
-app.get('/getLotById', jsonParser, async (req, res) => {
+app.get('/getLotById', async (req, res) => {
     try {
-        let lot = await getLotById(req.body.lotId);
+        let lot = await getLotById(Number(req.query.id));
         res.send(lot)
     } catch(e) {
         console.log(e)
